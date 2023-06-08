eb79010b518dba7e35ad4260e864c3d6778009af
ustasnov <ustasnov@gmail.com>
2023-06-09 02:52:48 +0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './dist')
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCSSExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new MiniCSSExtractPlugin()
+  ]
+}
